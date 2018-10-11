d499718b9b027311b48ee14efed07dc82bba68a4
Ashokkumar_V03 <Ashokkumar_V03@BLRKEC148369L.ad.infosys.com>
2018-10-11 23:14:05 +0530

    Sample comment to Spring Starter class

--- item/src/main/java/com/item/maintenance/ItemMaintenanceApplication.java
+++ item/src/main/java/com/item/maintenance/ItemMaintenanceApplication.java
@@ -3,6 +3,12 @@ package com.item.maintenance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Root class for starting the Spring Boot based application
+ * @author Ashokkumar_V03
+ *
+ */
+
 @SpringBootApplication
 public class ItemMaintenanceApplication {
 	public static void main(String[] args) {
